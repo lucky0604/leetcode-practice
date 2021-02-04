017391d0628f05d9264c975064476f8e33d865af
Lucky <lucky_soft@163.com>
2021-02-04 13:15:40 +0800

    feature(Linked List): add 430: Flatten a Multilevel Doubly Linked List

---
+++ src/main/java/com/fastdata/algorithm/medium/linked_list/FlatternAMultilevelDoublyLinkedList.java
@@ -0,0 +1,43 @@
+package com.fastdata.algorithm.medium.linked_list;
+
+import java.util.Stack;
+
+/**
+ * @Author: Lucky
+ * @Description: https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+ * @Date: create in 2/4/21 - 10:55 AM
+ */
+// TODO: to be understand
+public class FlatternAMultilevelDoublyLinkedList {
+    public Node flattern(Node head) {
+        if (head == null) return head;
+        Stack<Node> stack = new Stack<>();
+        Node tmp = head;
+        while (tmp != null) {
+            if (tmp.child != null) {
+                if (tmp.next != null) {
+                    stack.push(tmp.next);
+                }
+                tmp.next = tmp.child;
+                tmp.child.prev = tmp;
+                tmp.child = null;
+            }
+
+            if (tmp.next == null) {
+                break;
+            }
+            tmp = tmp.next;
+        }
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            tmp.next = node;
+            node.prev = tmp;
+            tmp = tmp.next;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+        }
+        return head;
+    }
+}
---
+++ src/main/java/com/fastdata/algorithm/medium/linked_list/Node.java
@@ -0,0 +1,13 @@
+package com.fastdata.algorithm.medium.linked_list;
+
+/**
+ * @Author: Lucky
+ * @Description:
+ * @Date: create in 2/4/21 - 10:55 AM
+ */
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+}
