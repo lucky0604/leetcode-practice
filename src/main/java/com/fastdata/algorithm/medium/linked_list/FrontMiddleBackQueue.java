c6b4657d492bdc0754889c185dd87c3a7dbfe53e
Lucky <lucky_soft@163.com>
2021-02-06 14:49:44 +0800

    fix

---
+++ src/main/java/com/fastdata/algorithm/medium/linked_list/FrontMiddleBackQueue.java
@@ -0,0 +1,34 @@
+package com.fastdata.algorithm.medium.linked_list;
+
+/**
+ * @Author: Lucky
+ * @Description: https://leetcode.com/problems/design-front-middle-back-queue/
+ * @Date: create in 2/6/21 - 10:12 AM
+ */
+public class FrontMiddleBackQueue {
+    public FrontMiddleBackQueue() {}
+
+    public void pushFront(int val) {
+
+    }
+
+    public void pushMiddle(int val) {
+
+    }
+
+    public void pushBack(int val) {
+
+    }
+
+    public int popFront() {
+
+    }
+
+    public int popMiddle() {
+
+    }
+
+    public int popBack() {
+
+    }
+}
