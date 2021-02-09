05929092027c2556f512219d6940e75391e2f165
Lucky <lucky_soft@163.com>
2021-02-09 12:12:09 +0800

    feature(Hash Table): add 884: Uncommon Words from Two Sentences

--- src/main/java/com/fastdata/algorithm/easy/hashtable/UncommonWordsFromTwoSentences.java
+++ src/main/java/com/fastdata/algorithm/easy/hashtable/UncommonWordsFromTwoSentences.java
@@ -3,6 +3,7 @@ package com.fastdata.algorithm.easy.hashtable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: Lucky
@@ -13,20 +14,24 @@ public class UncommonWordsFromTwoSentences {
     public String[] uncommonFromSentences(String A, String B) {
         String[] aArr = A.split("\\ ");
         String[] bArr = B.split("\\ ");
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < aArr.length; i ++) {
-            map.put(aArr[i], i);
+            map.put(aArr[i], map.getOrDefault(aArr[i], 0) + 1);
         }
-        List<String> ret = new ArrayList<>();
-        for (String item: bArr) {
-            if (!map.containsKey(item)) {
-                ret.add(item);
+        for (int i = 0; i < bArr.length; i ++) {
+            map.put(bArr[i], map.getOrDefault(bArr[i], 0) + 1);
+        }
+        List<String> uniqueArr = new ArrayList<>();
+        for (String str: map.keySet()) {
+            if (map.get(str) == 1) {
+                uniqueArr.add(str);
             }
         }
-        String[] res = new String[ret.size()];
-        for (int i = 0; i < ret.size(); i ++) {
-            res[i] = ret.get(i);
+        System.out.println(map);
+        String[] ret = new String[uniqueArr.size()];
+        for (int i = 0; i < uniqueArr.size(); i ++) {
+            ret[i] = uniqueArr.get(i);
         }
-        return res;
+        return ret;
     }
 }
