721db8db088881b33cecf7a892aaa89d34f9c881
Lucky <lucky_soft@163.com>
2021-02-13 13:07:30 +0800

    feature(Hash Table): add 242: Valid Anagram

---
+++ src/main/java/com/fastdata/algorithm/easy/hashtable/ValidAnagram.java
@@ -0,0 +1,26 @@
+package com.fastdata.algorithm.easy.hashtable;
+
+/**
+ * @Author: Lucky
+ * @Description: https://leetcode.com/problems/valid-anagram/
+ * @Date: created in 2021/2/13 - 12:30
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] sArray = new int[26];
+        int[] tArray = new int[26];
+
+        for (int i = 0; i < s.length(); i ++) {
+            sArray[s.charAt(i) - 97] ++;
+            tArray[t.charAt(i) - 97] ++;
+        }
+
+        for (int i = 0; i < 26; i ++) {
+            if (sArray[i] != tArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
