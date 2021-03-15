7a96d292340d078c641ca59a6dfade27154cbea4
Lucky <lucky_soft@163.com>
2021-03-15 11:14:07 +0800

    feature(Graph): add 1791: Find Center Of Star Graph

---
+++ src/main/java/com/fastdata/algorithm/medium/graph/FindCenterOfStarGraph.java
@@ -0,0 +1,27 @@
+package com.fastdata.algorithm.medium.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Lucky
+ * @Description:
+ * @Date: create in 3/15/21 - 10:22 AM
+ */
+public class FindCenterOfStarGraph {
+
+    public int findCenter(int[][] edges) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < edges.length; i ++) {
+            for (int j = 0; j < edges[i].length; j ++) {
+                map.put(edges[i][j], map.getOrDefault(edges[i][j], 0) + 1);
+            }
+        }
+        for (Integer i: map.keySet()) {
+            if (map.get(i).equals(edges.length)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
