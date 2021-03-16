e6d15b3cdaaac223f847ffb635100937bf2746af
Lucky <lucky_soft@163.com>
2021-03-16 11:31:46 +0800

    feature(Sort): add 1403: Minimum Subsequence in Non-Increasing Order

--- src/main/java/com/fastdata/algorithm/easy/sort/MinimumSubsequenceInNonIncreasingOrder.java
+++ src/main/java/com/fastdata/algorithm/easy/sort/MinimumSubsequenceInNonIncreasingOrder.java
@@ -1,5 +1,6 @@
 package com.fastdata.algorithm.easy.sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,7 +15,30 @@ public class MinimumSubsequenceInNonIncreasingOrder {
     }
 
     private static List<Integer> minSubsequence(int[] nums) {
+        // n < 100
+        int[] count = new int[101];
+        int sum = 0;
+        // counting the frequency of each of the elements
+        for (int i = 0; i < nums.length; i ++) {
+            sum += nums[i];
+            count[nums[i]] ++;
+        }
 
-        return null;
+        // if odd then add 1 to the offset
+        int offset = sum % 2 == 0 ? sum / 2 : (sum / 2) + 1;
+        List<Integer> ret = new ArrayList<>();
+        for (int i = count.length - 1; i >= 0; i --) {
+            // if the element more than once then run upto the frequency to account that
+            while (count[i] > 0) {
+                ret.add(i);
+                sum -= i;
+                if (sum < offset) {
+                    i = 0;
+                    break;
+                }
+                count[i] --;
+            }
+        }
+        return ret;
     }
 }
