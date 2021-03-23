64681be0e1720fd689ef8d35e7c2e4f08bbb76b2
Lucky <lucky_soft@163.com>
2021-03-23 10:47:01 +0800

    feature(Sort): add 1508: Range Sum of Sorted Subarray Sums

---
+++ src/main/java/com/fastdata/algorithm/medium/sort/RangeSumOfSortedSubarraySums.java
@@ -0,0 +1,34 @@
+package com.fastdata.algorithm.medium.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Author: Lucky
+ * @Description: https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/
+ * @Date: create in 3/23/21 - 10:05 AM
+ */
+public class RangeSumOfSortedSubarraySums {
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        List<Long> ret = new ArrayList<>();
+        long temp = 0, ans = 0, mod = 1000000007;
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = i; j < nums.length; j ++) {
+                temp = 0;
+                for (int k = i; k <= j; ++k) {
+                    temp += nums[k];
+                    ret.add(temp);
+                }
+            }
+        }
+        Collections.sort(ret);
+        -- left;
+        -- right;
+        for (int i = left; i <= right; i ++) {
+            ans = (ans + ret.get(i)) % mod;
+        }
+        return (int) ans;
+    }
+}
