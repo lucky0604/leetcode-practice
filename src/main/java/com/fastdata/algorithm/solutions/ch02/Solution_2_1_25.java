9898174f06548815dd42a1ffcdd87e3ddc153530
Lucky <lucky_soft@163.com>
2021-03-31 14:04:37 +0800

    feature(solution): add 2.1.25

---
+++ src/main/java/com/fastdata/algorithm/solutions/ch02/Solution_2_1_25.java
@@ -0,0 +1,45 @@
+package com.fastdata.algorithm.solutions.ch02;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Lucky
+ * @Description: Insertion sort without exchanges
+ * Develop an implementation of insertion sort
+ * that moves larger elements to the right one position with one array access per entry,
+ * rather than using exch(). Use SortCompare to evaluate the effectiveness of doing so.
+ * @Date: create in 3/31/21 - 12:48 PM
+ */
+public class Solution_2_1_25 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{9, 8, 0, 2, 1, 5};
+//        insertionSort(nums);
+        defaultInsertionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i ++) {
+            int tmp = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > tmp) {
+                nums[j + 1] = nums[j];
+                j --;
+            }
+            nums[j + 1] = tmp;
+        }
+    }
+
+    private static void defaultInsertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i ++) {
+            for (int j = i; j > 0; j --) {
+                 if (nums[j - 1] > nums[j]) {
+                     int tmp = nums[j];
+                     nums[j] = nums[j - 1];
+                     nums[j - 1] = tmp;
+                 }
+            }
+        }
+    }
+}
