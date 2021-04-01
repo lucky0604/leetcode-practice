411390b75b8a13734f9f51a8f851cac463fef29c
Lucky <lucky_soft@163.com>
2021-04-01 12:03:08 +0800

    feature(solution): add 2.1.26 solution

---
+++ src/main/java/com/fastdata/algorithm/solutions/ch02/Solution_2_1_26.java
@@ -0,0 +1,41 @@
+package com.fastdata.algorithm.solutions.ch02;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Lucky
+ * @Description:
+ * @Date: create in 4/1/21 - 11:53 AM
+ */
+public class Solution_2_1_26 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{8,4,2,5};
+        defaultInsertionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void defaultInsertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i ++) {
+            for (int j = i; j > 0; j --) {
+                if (nums[j] < nums[j - 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    private static void insertionSortWithPrimitiveType(Integer[] nums) {
+        for (int i = 1; i < nums.length; i ++) {
+            for (int j = i; j > 0; j --) {
+                if (nums[j] < nums[j - 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
