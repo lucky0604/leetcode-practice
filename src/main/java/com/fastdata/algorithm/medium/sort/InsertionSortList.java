31236ccd4f51a68aea1861a4c49a8ed4268d8a97
Lucky <lucky_soft@163.com>
2021-04-04 22:14:42 +0800

    feature(Sort): add 147: Insertion Sort List

---
+++ src/main/java/com/fastdata/algorithm/medium/sort/InsertionSortList.java
@@ -0,0 +1,34 @@
+package com.fastdata.algorithm.medium.sort;
+
+import com.fastdata.algorithm.medium.linked_list.ListNode;
+
+/**
+ * @Author: Lucky
+ * @Description: https://leetcode.com/problems/insertion-sort-list/
+ * @Date: created in 2021/4/4 - 21:58
+ */
+public class InsertionSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+        ListNode node = new ListNode(head.val);
+        head = head.next;
+        while (head != null) {
+            ListNode prev = null, curr = node;
+            while (curr != null && curr.val < head.val) {
+                prev = curr;
+                curr = curr.next;
+            }
+            ListNode tmp = new ListNode(head.val);
+            if (prev == null) {
+                tmp.next = curr;
+                node = tmp;
+            } else {
+                prev.next = tmp;
+                tmp.next = curr;
+            }
+            head = head.next;
+        }
+        return node;
+    }
+}
