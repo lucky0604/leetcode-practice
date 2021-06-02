3a73052a4ed4dd915c467d7f9a90dc91e527d883
Lucky <lucky_soft@163.com>
2021-06-02 11:29:41 +0800

    feature(Array): add Reverse Array

---
+++ src/main/java/com/fastdata/algorithm/medium/array/ReverseArray.java
@@ -0,0 +1,33 @@
+package com.fastdata.algorithm.medium.array;
+
+/**
+ * @Author: lucky
+ * @License: (C) Copyright
+ * @Contact: lucky_soft@163.com
+ * @Date: 6/2/21 10:40 AM
+ * @Version: 1.0
+ * @Description: https://leetcode.com/problems/rotate-array/
+ **/
+
+public class ReverseArray {
+
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        k %= length;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        int tmp;
+        while (left < right) {
+            tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left ++;
+            right --;
+        }
+    }
+}
