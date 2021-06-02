9a723cb1fc7c6a50c302c081637ec799a60e6e99
Lucky <lucky_soft@163.com>
2021-06-02 11:31:03 +0800

    fix Reverse Array into Rotate Array

--- src/main/java/com/fastdata/algorithm/medium/array/RotateArray.java
+++ src/main/java/com/fastdata/algorithm/medium/array/RotateArray.java
@@ -9,15 +9,15 @@ package com.fastdata.algorithm.medium.array;
  * @Description: https://leetcode.com/problems/rotate-array/
  **/
 
-public class ReverseArray {
+public class RotateArray {
 
     public void rotate(int[] nums, int k) {
         int length = nums.length;
         k %= length;
 
-        reverse(nums, 0, n - 1);
+        reverse(nums, 0, length - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(nums, k, length - 1);
     }
 
     private void reverse(int[] nums, int left, int right) {
